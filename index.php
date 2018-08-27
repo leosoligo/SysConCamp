9b13ac0a468c2f595cdf188aad91ca69ca74cef6
leosoligo <leosoligo@yahoo.com.br>
2018-08-27 11:30:06 -0400

    Teste
    Tetse

---
+++ index.php
@@ -0,0 +1,26 @@
+<html >
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Banco de Dados</title>    
+  <link rel="stylesheet" href="css/style.css">  
+</head>
+
+<body>
+  
+  <div class="login-page">
+  
+  <div class="form">
+
+    <form class="login-form" method="POST" action="login.php">
+      <b><h2>Acesso ao Sistema</h2></b>
+      <input type="number" name="senha" id="senha" pattern="[0-9]*" inputmode="numeric" style="-webkit-text-security: disc;" autofocus placeholder="senha"/>
+      <button type="submit">ACESSAR</button>
+    </form>
+		
+  </div>
+</div>
+  <script src="js/jquery.min.js"></script>
+  <script  src="js/index.js"></script>
+</body>
+</html>
