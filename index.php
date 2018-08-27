f770def2015222f498be6ac37ba9de53bd642ba1
leosoligo <leosoligo@yahoo.com.br>
2018-08-27 11:31:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,14 @@
   <title>Banco de Dados</title>    
   <link rel="stylesheet" href="css/style.css">  
 </head>
-
-<body>
-  
-  <div class="login-page">
-  
+<body>  
+  <div class="login-page">  
   <div class="form">
-
     <form class="login-form" method="POST" action="login.php">
       <b><h2>Acesso ao Sistema</h2></b>
       <input type="number" name="senha" id="senha" pattern="[0-9]*" inputmode="numeric" style="-webkit-text-security: disc;" autofocus placeholder="senha"/>
       <button type="submit">ACESSAR</button>
-    </form>
-		
+    </form>		
   </div>
 </div>
   <script src="js/jquery.min.js"></script>
