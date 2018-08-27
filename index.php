9ec809597ee8c42ff8c5c04bc75d32ba616d41b7
leosoligo <leosoligo@yahoo.com.br>
2018-08-27 11:58:13 -0400

    Alteração linha 12

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <div class="login-page">  
   <div class="form">
     <form class="login-form" method="POST" action="login.php">
-      <b><h2>Acesso ao Sistema</h2></b>
+      <b><h2>Acesso</h2></b>
       <input type="number" name="senha" id="senha" pattern="[0-9]*" inputmode="numeric" style="-webkit-text-security: disc;" autofocus placeholder="senha"/>
       <button type="submit">ACESSAR</button>
     </form>		
